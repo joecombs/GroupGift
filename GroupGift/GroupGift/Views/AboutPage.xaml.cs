4aa5c3d5786ba7c63734faafc138bf6632a0764f
joecombs <joe@joecombs.com>
2017-08-24 15:43:00 -0500

    ...future changes for about page

--- GroupGift/GroupGift/Views/AboutPage.xaml.cs
+++ GroupGift/GroupGift/Views/AboutPage.xaml.cs
@@ -19,7 +19,11 @@ namespace GroupGift.Views
             InitializeComponent();
             lblVersion.Text = string.Format("Version {0}", "1.0.0");
 
+            //faqs
+            AboutLinkList.Add(new AboutLinks { Text = "FAQs", Link = "http://joecombs.com/projects/groupgift/" });
+            //add once app is actually published on google play
             AboutLinkList.Add(new AboutLinks { Text = "Rate this App on Google Play", Link = "http://play.google.com/store/apps/details?id=com.google.android.apps.maps" });
+            //for now, just send the email
             AboutLinkList.Add(new AboutLinks { Text = "Send Feedback", Link = "mailto:joe@joecombs.com" });
         }
 
